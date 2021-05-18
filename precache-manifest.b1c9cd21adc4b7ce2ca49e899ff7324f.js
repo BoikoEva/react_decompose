12452efb1fd3795e3f2e2632c88d195ee71487f1
Eva <eva@MacBook-Air-Eva.local>
2021-05-18 11:59:26 +0300

    Updates

---
+++ precache-manifest.b1c9cd21adc4b7ce2ca49e899ff7324f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a49d19f346a3bb87be8750ebc8ffb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "780275f19a53cc4961b9",
+    "url": "./static/css/main.e20cc48d.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780275f19a53cc4961b9",
+    "url": "./static/js/main.fcee97a0.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
